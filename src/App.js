223f02e5d48b616e1a65844f187500f0a8e745bb
hongsoom <hongsoom@kakao.com>
2022-06-11 19:41:09 +0900

    create css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Singup from "./pages/Singup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import Add from "./pages/Add";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Singup" component={Singup} />
+        <Route exact path="/Add" component={Add} />
+        <Route exact path="/Edit" component={Edit} />
+        <Route exact path="/Detail" component={Detail} />
+      </Switch>
     </div>
   );
 }
