3e366464bc0569488da1c8b7e9d8ff8ff9fd95fd
hongsoom <hongsoom@kakao.com>
2022-06-14 09:21:25 +0900

    user commit

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 
 import { createBrowserHistory } from "history"
+import { connectRouter } from "connected-react-router"
 
 import thunk from "redux-thunk";
 import card from "./modules/card";
@@ -15,6 +16,7 @@ const enhancer = applyMiddleware(...middlewares);
 const rootReducer = combineReducers({ 
     card : card,
     user : user,
+    router: connectRouter(history),
 });
 
 const store = createStore(rootReducer, enhancer);
