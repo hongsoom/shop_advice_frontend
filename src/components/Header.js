78094be543457a7914ff7226649c581ec681991e
hongsoom <hongsoom@kakao.com>
2022-06-14 14:34:44 +0900

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Logo from '../assets/Logo.jpg';
 import '../css/Header.css';
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { LogoutFB } from "../redux/modules/user";
 
 const Header = () => {
+
     const history = useHistory();
+    const dispatch = useDispatch();
+
+    const is_login = useSelector((state) => state.user.is_login);
+
+    const Logout = () => {
+        dispatch(LogoutFB())
+    }
 
     return (
         <div className="Header-container">
@@ -13,8 +23,14 @@ const Header = () => {
                 <Link to="/"> 
                     <img src={Logo} alt="Logo" />
                 </Link>
-                <button className="Login_btn2" onClick={ () => history.push('/Login')}>로그인</button>
-                <button className="Singup_btn2" onClick={ () => history.push('/Signup')}>회원가입</button>
+                {is_login ? (
+                    <button className="Logout_btn2" onClick={Logout}>로그아웃</button>
+               ) : (
+                <>
+                    <button className="Login_btn2" onClick={ () => history.push('/Login')}>로그인</button>
+                    <button className="Singup_btn2" onClick={ () => history.push('/Signup')}>회원가입</button>
+                </>
+               ) }
             </div>
         </div>
     );
--- src/shared/Cookie.js
+++ src/shared/Cookie.js
@@ -8,7 +8,6 @@ const getCookie = (name) => {
   }
 };
 
-
 const setCookie = (name, value, exp = 5) => {
   let date = new Date();
   date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
