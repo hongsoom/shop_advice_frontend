23485cc3f99029f5e6e9651b502881a16f440210
hongsoom <hongsoom@kakao.com>
2022-06-15 14:36:39 +0900

    is_login delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,10 @@ import React from 'react';
 import '../css/Home.css';
 import { useHistory } from 'react-router-dom';
 import Card from "../components/card";
-import { useSelector, useDispatch } from "react-redux";
-
 
 const Home = (props) => {
     const history = useHistory();
-    const dispatch = useDispatch();
-    const is_login = useSelector((state) => state.user.is_login);   
-    
+
     // 드롭다운 관련
     const [category, setCategory] = React.useState('');
 
@@ -20,9 +16,7 @@ const Home = (props) => {
     return (        
         <div className="Home-container">
             <div className="Home-content">
-                {is_login ? (
                     <button onClick={() => history.push('/Add')}>작성하러가기</button>
-                ) : null }
                 <div className='select'>
                     {/* 드롭박스 */}
                     <select value={category}
