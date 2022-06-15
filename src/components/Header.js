030466085fad89fdcbaadc24f3ea7f81f4d89c3b
hongsoom <hongsoom@kakao.com>
2022-06-15 09:41:00 +0900

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,44 +5,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { LogoutFB } from "../redux/modules/user";
-import { getCookie } from "../shared/Cookie";
 
 const Header = (props) => {
 
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const is_login = useSelector((state) => state.user.is_login);
-    console.log(is_login)
-
-    const cookie = getCookie("is_login");
-    const token = localStorage.getItem("token")
     const nickname = localStorage.getItem("nickname")
+    const token = localStorage.getItem("token")
 
     const Logout = () => {
         dispatch(LogoutFB())
     }
 
+    if (token) {
     return (
         <div className="Header-container">
             <div className="Header-content">
                 <Link to="/"> 
                     <img src={Logo} alt="Logo" />
                 </Link>
-                {is_login ? (
-                <>
-                    <p>{nickname}님 환영합니다!</p>
+                    <p className="nickname_btn2">{nickname}님 환영합니다!</p>
                     <button className="Logout_btn2" onClick={Logout}>로그아웃</button>
-                </>        
-                    ) : (
-                <>
-                    <button className="Login_btn2" onClick={ () => history.push('/Login')}>로그인</button>
-                    <button className="Singup_btn2" onClick={ () => history.push('/Signup')}>회원가입</button>
-                </>
-               ) }
             </div>
         </div>
-        );
+        )
+    } else {
+        return (
+            <div className="Header-container">
+                <div className="Header-content">
+                    <Link to="/"> 
+                        <img src={Logo} alt="Logo" />
+                    </Link>
+                        <button className="Login_btn2" onClick={ () => history.push('/Login')}>로그인</button>
+                        <button className="Singup_btn2" onClick={ () => history.push('/Signup')}>회원가입</button>
+                </div>
+            </div>
+        )    
     }
+}
 
 export default Header;
